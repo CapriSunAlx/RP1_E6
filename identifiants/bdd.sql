65fbd7d97cba87c342ff6ae895f05231d1c7ed3d
CapriSunAlx <137153093+CapriSunAlx@users.noreply.github.com>
2025-04-18 10:41:04 +0400

    Update bdd.sql

--- identifiants/bdd.sql
+++ identifiants/bdd.sql
@@ -72,8 +72,6 @@ Structure de la table `favoris`
 ******************************/
 
 DROP TABLE IF EXISTS `favoris`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `favoris` (
   `utilisateur_id` int NOT NULL,
   `post_id` int NOT NULL,
